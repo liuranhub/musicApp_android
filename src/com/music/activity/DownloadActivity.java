689edbc8a07b6ae355b859661c15c0a06444729b
lr635934333 <635934333@qq.com>
2015-11-14 23:30:17 +0800

    修改注释

--- src/com/music/activity/DownloadActivity.java
+++ src/com/music/activity/DownloadActivity.java
@@ -9,7 +9,7 @@ import com.music.musicapp.R;
 public class DownloadActivity extends Activity {
 
 	/**
-	 * 显示所有下载的歌曲的Activity
+	 * 显示所有下载的歌曲的Activity（包括正在下载和已经下载）
 	 * */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
